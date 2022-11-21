85f2fc157692337ced14530fb709d5d12e078f5a
MTX-6oX <40661905+MTX-6oX@users.noreply.github.com>
2022-11-21 17:51:39 +0100

    working own lead pretty nice

--- quantum/rgb_matrix/animations/solid_reactive_nexus.h
+++ quantum/rgb_matrix/animations/solid_reactive_nexus.h
@@ -15,12 +15,13 @@ static HSV SOLID_REACTIVE_NEXUS_math(HSV hsv, int16_t dx, int16_t dy, uint8_t di
     uint16_t effect = tick - dist;
     if (effect > 255) effect = 255;
     if (dist > 72) effect = 255;
+    if(effect!=255) dprintf("%u\n", effect);
     if ((dx > 8 || dx < -8) && (dy > 8 || dy < -8)) effect = 255;
 #            ifdef RGB_MATRIX_SOLID_REACTIVE_GRADIENT_MODE
     hsv.h = scale16by8(g_rgb_timer, add8(rgb_matrix_config.speed, 1) >> 6);
 #            endif
     hsv.v = qadd8(hsv.v, 255 - effect);
-    hsv.h = rgb_matrix_config.hsv.h + dy / 4;
+    hsv.h = qadd8(hsv.h, 255 - effect); //rgb_matrix_config.hsv.h + dy / 4;
     return hsv;
 }
 
