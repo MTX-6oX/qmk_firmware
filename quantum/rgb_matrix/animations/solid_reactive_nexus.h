5f08cb4583d59074146f5766ce541eed6752a712
MTX-6oX <40661905+MTX-6oX@users.noreply.github.com>
2022-11-21 19:18:42 +0100

    cleanup

--- quantum/rgb_matrix/animations/solid_reactive_nexus.h
+++ quantum/rgb_matrix/animations/solid_reactive_nexus.h
@@ -13,23 +13,22 @@ RGB_MATRIX_EFFECT(SOLID_REACTIVE_MULTINEXUS)
 
 static HSV SOLID_REACTIVE_NEXUS_math(HSV hsv, int16_t dx, int16_t dy, uint8_t dist, uint16_t tick) {
     uint16_t effect = tick - dist;
-    uint16_t nexusHue = 127;
+    uint16_t nexusHue = 127; //TARGET COLOR
     if (effect > 255) effect = 255;
     if (dist > 72) effect = 255;
-    //if(effect!=255) dprintf("%u\n", effect);
+    //if(effect!=255) dprintf("%u\n", effect); //DEBUG
     if ((dx > 8 || dx < -8) && (dy > 8 || dy < -8)) effect = 255;
-#            ifdef RGB_MATRIX_SOLID_REACTIVE_GRADIENT_MODE
-    hsv.h = scale16by8(g_rgb_timer, add8(rgb_matrix_config.speed, 1) >> 6);
-#            endif
+    #ifdef RGB_MATRIX_SOLID_REACTIVE_GRADIENT_MODE
+        hsv.h = scale16by8(g_rgb_timer, add8(rgb_matrix_config.speed, 1) >> 6);
+    #endif
     hsv.v = qsub8(hsv.v, scale8(69,255 - effect)); //STOMP effect
-    //hsv.h = qsub8(hsv.h, (255 - effect)/2);
     if(hsv.h>nexusHue) {
+        //go down with h if stomp hue is smaller
         hsv.h = qsub8(hsv.h, scale8((hsv.h-nexusHue),255-effect));
     } else {
+        //go up with h if stomp hue is bigger
         hsv.h = qadd8(hsv.h, scale8((nexusHue-hsv.h),255-effect));
     }
-
-    //if (effect < 255) hsv.h = 127; //rgb_matrix_config.hsv.h + dy / 4;
     return hsv;
 }
 
